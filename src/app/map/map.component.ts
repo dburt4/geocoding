6e9ac3a4975e08c8c763143e108ccc3711d2a221
Daniel Burt <daniel.burt99@gmail.com>
2019-06-07 00:49:08 -0600

    Did some more stuff

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AgmCoreModule } from '@agm/core';
+import { GeolocationService } from '../services/geolocation.service';
 
 @Component({
   selector: 'app-map',
@@ -11,16 +12,36 @@ export class MapComponent implements OnInit {
   startingLong = -111.646325;
   mapType = 'satellite';
   markers = [
-    { lat: 40.327730, long: -111.646325, alpha: 0.4 },
+    { lat: 40.327730, long: -111.646325, alpha: 1 },
   ];
   zoom = 13;
+  selectedMarker; 
+  currentPostion = {
+    lat: 0, long: 0
+  }; 
 
-  constructor() { }
+  constructor(private geolocationService: GeolocationService) { }
 
   ngOnInit() {
+    this.geolocationService.getPosition().subscribe(
+      (pos: Position) => {
+          this.currentPostion = {
+            lat:  +(pos.coords.latitude),
+            long: +(pos.coords.longitude)
+          };
+      });
   }
 
   addMarker(lat: number, long: number) {
+    console.log("Adding marker!", lat, long);
     this.markers.push({ lat, long, alpha: 0.4 });
+  }
+
+  setSelectedMarker(event) {
+    this.selectedMarker = {
+      lat: event.latitude,
+      long: event.longitude,
+      alpha: 0.4
+    };
   }
 }
